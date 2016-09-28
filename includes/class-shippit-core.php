535a67f0df53dfe2db8e314213ffaec00af82fe4
Matthew Muscat <matthew@mamis.com.au>
2016-09-29 09:25:59 +1000

    Woocommerce Shippit v1.2.6
    - update the quote request to include item level details
    - add length, width and height dimentions to the quote and order requests
    - add conversion functionality for dimension + weight units
    
    Signed-off-by: Matthew Muscat <matthew@mamis.com.au>

--- includes/class-shippit-core.php
+++ includes/class-shippit-core.php
@@ -19,7 +19,7 @@ class Mamis_Shippit_Core
     /**
      * Version.
      */
-    public $version = '1.2.5';
+    public $version = '1.2.6';
     public $id = 'mamis_shippit';
 
     // The shipping methods
