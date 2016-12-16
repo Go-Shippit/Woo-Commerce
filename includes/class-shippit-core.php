de25e721c959183d19fdb7866286103f68779244
Matthew Muscat <matthew@mamis.com.au>
2016-12-16 11:38:26 +1100

    WooCommerce Shippit v1.2.9
    - [Change] API timeout updates
    - [Bugfix] Resolve an issue with product dimentions when syncing orders
    
    Signed-off-by: Matthew Muscat <matthew@mamis.com.au>

--- includes/class-shippit-core.php
+++ includes/class-shippit-core.php
@@ -19,7 +19,7 @@ class Mamis_Shippit_Core
     /**
      * Version.
      */
-    public $version = '1.2.7';
+    public $version = '1.2.9';
     public $id = 'mamis_shippit';
 
     // The shipping methods
