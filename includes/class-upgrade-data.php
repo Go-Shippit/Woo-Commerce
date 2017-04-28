0b8c20ab6b34fe1fc2ddd8c9e38ba87b23139368
Hardik Thakkar <hardik@mamis.com.au>
2017-04-28 19:11:36 +1000

    Removed unwanted variables
    Signed-off-by: Hardik Thakkar <hardik@mamis.com.au>

--- includes/class-upgrade-data.php
+++ includes/class-upgrade-data.php
@@ -2,14 +2,14 @@
 
 class Upgrade_Data
 {
-	const OLD_OPTIONS_PREFIX = 'woocommerce_mamis_shippit_';
 	const NEW_OPTIONS_PREFIX = 'wc_settings_shippit_global_';
+
 	public function upgrade()
 	{
 		$old_version = get_option('wc_shippit_version', 0);
 		if (version_compare($old_version, MAMIS_SHIPPIT_VERSION) < 0) {
             //code to upgrade to 1.2.12
-            $this->upgrade_1212($installer);
+            $this->upgrade_1212();
         }
         else
         {
