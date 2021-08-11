10d2a6f46b2c7540db722aabb80192039a903194
Jason <jbolger@techcore.com.au>
2021-08-11 21:19:50 +1000

    Updated logger to use WC_Logger

--- includes/class-shippit-log.php
+++ includes/class-shippit-log.php
@@ -18,17 +18,35 @@ class Mamis_Shippit_Log
 {
     public function add($errorType, $message = null, $metaData = null, $severity = 'info')
     {
-        // If debug mode is active, log all info serverities, otherwise log only errors
+        // If debug mode is active, log all info severities, otherwise log only errors
         if (get_option('wc_settings_shippit_debug') == 'yes' || $severity == 'error') {
-            error_log('-- ' . $errorType . ' --');
+            
+            // Updated to use WC_Logger
+            $logger = wc_get_logger();
+            $loggerContext = array( 'source' => 'shippit' );
 
-            if (!is_null($message)) {
-                error_log($message);
+            if ($severity == 'error') {
+                $logger->error('-- ' . $errorType . ' --', $loggerContext );
+            
+                if (!is_null($message)) {
+                    $logger->error($message, $loggerContext );
+                }
+    
+                if (!is_null($metaData)) {
+                    $logger->error(json_encode($metaData), $loggerContext );
+                }
+            } else {
+                $logger->info('-- ' . $errorType . ' --', $loggerContext );
+            
+                if (!is_null($message)) {
+                    $logger->info($message, $loggerContext );
+                }
+    
+                if (!is_null($metaData)) {
+                    $logger->info(json_encode($metaData), $loggerContext );
+                }
             }
 
-            if (!is_null($metaData)) {
-                error_log(json_encode($metaData));
-            }
         }
     }
 
@@ -44,6 +62,9 @@ class Mamis_Shippit_Log
     */
     public function exception($exception)
     {
-        error_log($exception->getMessage());
+                    // Updated to use WC_Logger
+                    $logger = wc_get_logger();
+                    $loggerContext = array( 'source' => 'shippit' );
+                    $logger->error($exception->getMessage());
     }
 }
