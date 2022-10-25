2845c04288a0df70c4af5278c836401e40e6d994
Matthew Muscat <matthew@mamis.com.au>
2022-10-25 17:38:52 +1100

    Fix version number metadata
    - Indicate support for WooCommerce v7.0.0
    - Bump version number to v1.7.1

--- woocommerce-shippit.php
+++ woocommerce-shippit.php
@@ -2,15 +2,15 @@
 /*
  * Plugin Name:             WooCommerce Shippit
  * Description:             WooCommerce Shippit
- * Version:                 1.6.7
+ * Version:                 1.7.1
  * Author:                  Shippit Pty Ltd
  * Author URL:              http://www.shippit.com
  * Text Domain:             woocommerce-shippit
  * WC requires at least:    2.6.0
- * WC Tested Up To:         6.8.2
+ * WC Tested Up To:         7.0.0
  */
 
-define('MAMIS_SHIPPIT_VERSION', '1.6.7');
+define('MAMIS_SHIPPIT_VERSION', '1.7.1');
 
 // import core classes
 include_once('includes/class-shippit-helper.php');
